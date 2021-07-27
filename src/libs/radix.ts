9e132fe3ad5d5ef5cb398d7b11644b6fceb1220c
Lycolia Rizzim <lycolia@lycolia.info>
2021-07-27 21:09:46 +0900

    decode処理の簡略化

--- src/libs/radix.ts
+++ src/libs/radix.ts
@@ -5,37 +5,8 @@
  * @returns
  */
 export const decode = (value: number, baseValue: number) => {
-  const signMap: { [key: number]: string } = {
-    10: 'A',
-    11: 'B',
-    12: 'C',
-    13: 'D',
-    14: 'E',
-    15: 'F',
-    16: 'G',
-    17: 'H',
-    18: 'I',
-    19: 'J',
-    20: 'K',
-    21: 'L',
-    22: 'M',
-    23: 'N',
-    24: 'O',
-    25: 'P',
-    26: 'Q',
-    27: 'R',
-    28: 'S',
-    29: 'T',
-    30: 'U',
-    31: 'V',
-    32: 'W',
-    33: 'X',
-    34: 'Y',
-    35: 'Z',
-  };
-
   return baseValue > 10 && baseValue < 37 && value > 9 && value < 36
-    ? signMap[value]
+    ? String.fromCharCode(value + 55)
     : value.toString();
 };
 
